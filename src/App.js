335ce1fc3e132297fb8fb692a236fea34d6532d7
BarnoMamadzhanova <b@mamadzhanova.com>
2024-04-16 01:40:18 +0200

    calculating statistics as Derived State

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ export default function App() {
         onDeleteItem={handleDeleteitem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -52,7 +52,6 @@ function Form({ onAddItems }) {
     if (!description) return;
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
-    console.log(newItem);
 
     onAddItems(newItem);
 
@@ -119,10 +118,26 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🪄</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPackedItems = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPackedItems / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>👜 You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go ✈️"
+          : `👜 You have ${numItems} items on your list, and you already packed
+        ${numPackedItems} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
